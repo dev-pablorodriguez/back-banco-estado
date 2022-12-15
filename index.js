68c806731f5484f80424811963eced40ce1f6263
Pablo Rodríguez <pablo.rodriguez.musre@gmail.com>
2022-12-15 20:22:29 -0300

    Variables entorno

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+require('dotenv').config();
+
+const port = process.env.PORT;
 
 //create express server
 const app = express();
@@ -9,6 +12,6 @@ app.get('/', (req, res) => {
 })
 
 //Listen requests
-app.listen(3000, () => {
-    console.log(`Server running on port ${ 3000 }`);
+app.listen(port, () => {
+    console.log(`Server running on port ${ port }`);
 })
